723de52d43e06723193c43389741e2a27cfdb78b
Hoàng Long <hoanghaidangtc2003@gmail.com>
2025-08-20 22:30:47 +0700

    Lap full set vao player

---
+++ Assets/_Scripts/MenuPlayerPrefs/FullSet/FullSetInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class FullSetInfo
+{
+    public GameObject hairFull;
+    public GameObject shieldFull;
+    public GameObject tailFull;
+    public GameObject wingFull;
+    public Material pantFull;
+    public Material initialFull;
+
+
+}
